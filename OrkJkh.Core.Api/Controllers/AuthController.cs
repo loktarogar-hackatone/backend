0587a766e24904693f0e501da2f12cbbce6702bf
EatsLemons <frimko@gmail.com>
2019-04-06 10:29:32 +0400

    config gi

---
+++ OrkJkh.Core.Api/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrkJkh.Core.Api.Controllers
+{
+	public class AuthController : ControllerBase
+	{
+		[ApiController]
+		public AuthController()
+		{
+		}
+	}
+}
